d4e16197a50323c9b17e8ee83efe64fd892bce1d
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2023-11-13 23:33:25 -0800

    Tasks in printf

--- print_uns.c
+++ print_uns.c
@@ -9,39 +9,39 @@
 
 int print_unsigned(va_list args)
 {
-        unsigned int m = va_arg(args, unsigned int);
-        int num, last = m % 10, d, e = 1;
-        int  a = 1;
+	unsigned int m = va_arg(args, unsigned int);
+	int num, last = m % 10, d, e = 1;
+	int  a = 1;
 
-        m = m / 10;
-        num = m;
+	m = m / 10;
+	num = m;
 
-        if (last < 0)
-        {
-                _putchar('-');
-                num = -num;
-                m = -m;
-                last = -last;
-                a++;
-        }
-        if (num > 0)
-        {
-                while (num / 10 != 0)
-                {
-                        e = e * 10;
-                        num = num / 10;
-                }
-                num = m;
-                while (e > 0)
-                {
-                        d = num / e;
-                        _putchar(d + '0');
-                        num = num - (d * e);
-                        e = e / 10;
-                        a++;
-                }
-        }
-        _putchar(last + '0');
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		m = -m;
+		last = -last;
+		a++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			e = e * 10;
+			num = num / 10;
+		}
+		num = m;
+		while (e > 0)
+		{
+			d = num / e;
+			_putchar(d + '0');
+			num = num - (d * e);
+			e = e / 10;
+			a++;
+		}
+	}
+	_putchar(last + '0');
 
-        return (a);
+	return (a);
 }
